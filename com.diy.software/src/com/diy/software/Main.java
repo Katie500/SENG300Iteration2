db161ad884f003c7fa9cc2e374e7344f03a5d34b
Katie500 <katherine.knauss@ucalgary.ca>
2022-11-01 11:45:16 -0600

    Change git layout

---
+++ com.diy.software/src/com/diy/software/Main.java
@@ -0,0 +1,9 @@
+package com.diy.software;
+
+import com.diy.hardware.DoItYourselfStation;
+
+public class Main {
+    public static void main(String[] args) {
+        new DoItYourselfStation();
+    }
+}
