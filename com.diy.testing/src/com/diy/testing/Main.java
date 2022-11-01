c27e0823e54384f8cacca29365f074db45cb8e85
Katie500 <katherine.knauss@ucalgary.ca>
2022-11-01 12:05:15 -0600

    Reformatting git to include two different projects for a total of three projects: testing, software, and hardware (named seng project instead of hardware)

---
+++ com.diy.testing/src/com/diy/testing/Main.java
@@ -0,0 +1,9 @@
+package com.diy.testing;
+
+import com.diy.hardware.DoItYourselfStation;
+
+public class Main {
+    public static void main(String[] args) {
+        new DoItYourselfStation();
+    }
+}
