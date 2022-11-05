0de008ee8d3452a5ab34a5d41d192dfa5fdf1e86
Muhammad Adil <muhammad.adil3@cs.ucalgary.ca>
2022-11-05 01:48:20 -0600

    intialising a skeleton of test file for item scanning.

--- com.diy.software.test/src/com/diy/software/testing/Main.java
+++ com.diy.software.test/src/com/diy/software/testing/Main.java
@@ -1,7 +1,43 @@
 package com.diy.software.testing;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import package com.jimmyelectronics.necchil;
+
+import org.junit.*;
+import com.jimmyselectronics.AbstractDevice;
+import com.jimmyselectronics.AbstractDeviceListener;
+import com.jimmyselectronics.opeechee.*;
+
+public class addItemsByScanTest {
+
+    public BarcodedItem item1;
+    public BarcodedItem item2;
+
+    @Before
+    public void setup(){
+        scanner = new BarcodeScanner();
+        barcode = new Barcode(new Numeral[] { Numeral.one, Numeral.one, Numeral.one, Numeral.one }); // 1111
+        item = new BarcodedItem(barcode, 10);
+        item2 = new BarcodedItem(null, 10);
     }
+
+    @Test(expected = weightDiscrepancyException)
+    public void weightDiscrepancyTest(){
+        //Test to scan an item with valid baarcode, once item added to cart,
+    // weight in bagging area does not correspond to expectations
+    }
+
+
+    @Test
+    public void addedToCartTest(){
+        // Successful addition of item by scan test
+        // Include assert test for correct weight of bagging area after addition of scanned item
+    }
+
+    @Test
+    public void notRegisteredTest(){
+
+    }
+
+
+
 }
