4cb61224b9b0fe0fb166a10848f6c79afb88b61c
Katie500 <katherine.knauss@ucalgary.ca>
2022-11-04 23:33:26 -0600

    Changing testing to test so it fits with the assignment specifications

--- com.diy.software/src/com/diy/software/Gui.java
+++ com.diy.software/src/com/diy/software/Gui.java
@@ -2,8 +2,9 @@ package com.diy.software;
 
 import com.diy.hardware.DoItYourselfStation;
 
-public class Main {
+public class Gui {
     public static void main(String[] args) {
         new DoItYourselfStation();
+        //create gui here
     }
 }
