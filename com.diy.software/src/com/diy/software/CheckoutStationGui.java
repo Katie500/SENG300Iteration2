c76d85747d29b417fee091d2f517e434818f9a0c
Katie500 <katherine.knauss@ucalgary.ca>
2022-11-05 11:44:32 -0600

    Adding base gui files

--- com.diy.software/src/com/diy/software/CheckoutStationGui.java
+++ com.diy.software/src/com/diy/software/CheckoutStationGui.java
@@ -1,8 +1,10 @@
 package com.diy.software;
 
 import com.diy.hardware.DoItYourselfStation;
+import javax.swing.*;
 
-public class Gui {
+public class CheckoutStationGui {
+    JFrame frame;
     public static void main(String[] args) {
         new DoItYourselfStation();
         //create gui here
