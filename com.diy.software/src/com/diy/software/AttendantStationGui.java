1dbee0667d024d36b64db75eae965c21313d5cdb
Anika Achari <anika3040@gmail.com>
2022-11-05 20:26:02 -0600

    GUI attendant screen

--- com.diy.software/src/com/diy/software/AttendantStationGui.java
+++ com.diy.software/src/com/diy/software/AttendantStationGui.java
@@ -1,12 +1,43 @@
 package com.diy.software;
 
-import com.diy.hardware.DoItYourselfStation;
-import javax.swing.*;
+import java.awt.EventQueue;
 
-public class AttendantStationGui {
-    JFrame frame;
-    public static void main(String[] args) {
-        new DoItYourselfStation();
-        //create gui here
-    }
+import javax.swing.JFrame;
+
+public class AttendantScreen {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AttendantScreen window = new AttendantScreen();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public AttendantScreen() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
 }
