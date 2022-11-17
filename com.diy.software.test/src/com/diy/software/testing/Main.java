ddd539bc7b88402054dc6935ca8101c91fc03293
nd <nd@localhost>
2022-11-17 11:29:17 -0700

    commented test

--- com.diy.software.test/src/com/diy/software/testing/Main.java
+++ com.diy.software.test/src/com/diy/software/testing/Main.java
@@ -6,28 +6,29 @@ package com.diy.software.testing;
 import org.junit.*;
 import com.jimmyselectronics.AbstractDevice;
 import com.jimmyselectronics.AbstractDeviceListener;
+import com.jimmyselectronics.necchi.BarcodeScanner;
 import com.jimmyselectronics.opeechee.*;
-import com.jimmyselectronics.nechi.*;
+import com.jimmyselectronics.necchi.*;
 
-public class addItemsByScanTest {
+public class Main {
 
     public BarcodedItem item1;
     public BarcodedItem item2;
 
     @Before
     public void setup(){
-        scanner = new BarcodeScanner();
-        barcode = new Barcode(new Numeral[] { Numeral.one, Numeral.one, Numeral.one, Numeral.one }); // 1111
-        barcode2 = new Barcode(new Numeral[] { Numeral.one, Numeral.two, Numeral.three, Numeral.four }); // 1234
-        item = new BarcodedItem(barcode, 10);
+        BarcodeScanner scanner = new BarcodeScanner();
+        Barcode barcode = new Barcode(new Numeral[] { Numeral.one, Numeral.one, Numeral.one, Numeral.one }); // 1111
+        Barcode barcode2 = new Barcode(new Numeral[] { Numeral.one, Numeral.two, Numeral.three, Numeral.four }); // 1234
+        //item = new BarcodedItem(barcode, 10);
         item2 = new BarcodedItem(barcode2, 10);
     }
 
-    @Test(expected = weightDiscrepancyException)
-    public void weightDiscrepancyTest(){
-        //Test to scan an item with valid baarcode, once item added to cart,
-    // weight in bagging area does not correspond to expectations
-    }
+//    @Test(expected = weightDiscrepancyException)
+//    public void weightDiscrepancyTest(){
+//        //Test to scan an item with valid baarcode, once item added to cart,
+//    // weight in bagging area does not correspond to expectations
+//    }
 
 
     @Test
@@ -47,45 +48,42 @@ public class addItemsByScanTest {
 
 
 
-}
------------------------------------------------------------------------------------------------------------------
-
-public class payWithCreditCardTest(){
-    private CardReader reader;
-    private Card card;
-    private String pin;
-
-    @Before
-    public void setup(){
-        pin = "1234";
-        reader = new CardReader();
-        card = new Card("Visa", "1234", "Tony", "333", pin, true, true);
-    }
-
-    @Test
-    public void blockCardTest(){
-        // Block card if wrong pin entered three times
-    }
-
-    @Test
-    public void cardDeniedTest(){
-        // Case where the transaction fails due to issues with the bank
-    }
-
-    @Test
-    public void cardInsertTest(){
-        // Test ability to read a card and its details
-    }
-
-    @Test
-    public void successfulPaymentTest(){
-        // transaction goes through and the amount due is decremented.
-        // Maybe test for printout of receipt or formulaation of receipt
-    }
-
-    @Test
-    public void cardRemovedTest(){
-        // Check for any card details once card is removed.
-        // Most details should not be saved typically. For security / privacy reasons
-    }
+//public class payWithCreditCardTest(){
+//    private CardReader reader;
+//    private Card card;
+//    private String pin;
+//
+//    @Before
+//    public void setup(){
+//        pin = "1234";
+//        reader = new CardReader();
+//        card = new Card("Visa", "1234", "Tony", "333", pin, true, true);
+//    }
+//
+//    @Test
+//    public void blockCardTest(){
+//        // Block card if wrong pin entered three times
+//    }
+//
+//    @Test
+//    public void cardDeniedTest(){
+//        // Case where the transaction fails due to issues with the bank
+//    }
+//
+//    @Test
+//    public void cardInsertTest(){
+//        // Test ability to read a card and its details
+//    }
+//
+//    @Test
+//    public void successfulPaymentTest(){
+//        // transaction goes through and the amount due is decremented.
+//        // Maybe test for printout of receipt or formulaation of receipt
+//    }
+//
+//    @Test
+//    public void cardRemovedTest(){
+//        // Check for any card details once card is removed.
+//        // Most details should not be saved typically. For security / privacy reasons
+//    }
 }
