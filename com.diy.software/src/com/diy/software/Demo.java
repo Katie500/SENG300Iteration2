f858f3630712e79253122d972ed86d0a22e1b779
Katie500 <katherine.knauss@ucalgary.ca>
2022-11-22 11:20:58 -0700

    Adding Debit Card Selection Gui for selecting card

--- com.diy.software/src/com/diy/software/Demo.java
+++ com.diy.software/src/com/diy/software/Demo.java
@@ -6,6 +6,7 @@ import com.diy.hardware.external.CardIssuer;
 import com.diy.hardware.external.ProductDatabases;
 import com.diy.simulation.Customer;
 import com.jimmyselectronics.necchi.Barcode;
+//import com.jimmyselectronics.necchi.BarcodedItem;
 import com.jimmyselectronics.necchi.BarcodedItem;
 import com.jimmyselectronics.necchi.Numeral;
 import com.jimmyselectronics.opeechee.Card;
