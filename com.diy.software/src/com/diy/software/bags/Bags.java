64158a786b1b909966aba11288e58cb0610abef7
besim.zurnaxhiu <besim.zurnaxhiu@ucalgary.ca>
2022-11-23 10:10:42 -0700

    added bags functionality Merge branch 'master' of https://github.com/Katie500/SENG300Iteration2

---
+++ com.diy.software/src/com/diy/software/bags/Bags.java
@@ -0,0 +1,32 @@
+package com.diy.software.bags;
+
+public class Bags {
+	private final long BAG_PRICE = 1;
+	private int totalBagsSelected;
+	private boolean noBags;
+	
+	public Bags () {
+		noBags = false;
+		totalBagsSelected = 0;
+	}
+	
+	public long getBagPrice() {
+		return BAG_PRICE;
+	}
+	
+	public int getTotalBagsSelected() {
+		return totalBagsSelected;
+	}
+	
+	public void addBag() {
+		totalBagsSelected = totalBagsSelected + 1;
+	}
+	
+	public boolean ownBagsOrNot() {
+		return noBags;
+	}
+	
+	public void updateOwnBagsOrNot(boolean choice) {
+		noBags = choice;
+	}
+}
