afa084fb6943eb0f6f3e446bbf8966a16a07999b
SIbouy <mengmegan@outlook.com>
2022-11-22 19:20:07 -0700

    Adding cash payment functionalities

--- com.diy.software/src/com/diy/software/controllers/BanknotePaymentController.java
+++ com.diy.software/src/com/diy/software/controllers/BanknotePaymentController.java
@@ -3,6 +3,7 @@ package com.diy.software.controllers;
 import com.diy.software.DoItYourselfStationLogic;
 
 public class BanknotePaymentController {
+
     private long stored_value = 0;
     private DoItYourselfStationLogic stationLogic;
 }
--- com.diy.software/src/com/diy/software/controllers/CoinPaymentController.java
+++ com.diy.software/src/com/diy/software/controllers/CoinPaymentController.java
@@ -1,5 +1,4 @@
 package com.diy.software.controllers;
 
 public class CoinPaymentController {
-
 }
