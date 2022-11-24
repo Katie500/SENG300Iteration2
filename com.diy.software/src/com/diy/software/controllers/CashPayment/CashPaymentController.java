c45558c4dd8beadb251a192d533fefe259d94df0
SIbouy <mengmegan@outlook.com>
2022-11-24 16:59:13 -0700

    update settings for currency

--- com.diy.software/src/com/diy/software/controllers/CashPayment/CashPaymentController.java
+++ com.diy.software/src/com/diy/software/controllers/CashPayment/CashPaymentController.java
@@ -4,20 +4,29 @@ import com.diy.software.DoItYourselfStationLogic;
 
 public class CashPaymentController {
     private DoItYourselfStationLogic stationLogic;
-    private BanknotePaymentController banknotePaymentController;
-    private CoinPaymentController coinPaymentController;
+    private BanknotePaymentController banknotes;
+    private CoinPaymentController coins;
 
-    public CashPaymentController(DoItYourselfStationLogic stationLogic,BanknotePaymentController banknotePaymentController,CoinPaymentController coinPaymentController){
+    public CashPaymentController(DoItYourselfStationLogic stationLogic,BanknotePaymentController banknotes,CoinPaymentController coins){
         this.stationLogic = stationLogic;
-        this.banknotePaymentController = banknotePaymentController;
-        this.coinPaymentController = coinPaymentController;
+        this.banknotes = banknotes;
+        this.coins = coins;
     }
 
-    public long payWithCash(){
+    public long payWithCash() throws Exception {
+
         long charge = stationLogic.productController.getTotal();
-        long totalBanknotes = banknotePaymentController.getTotalBanknotes();
-        long totalCoins = coinPaymentController.getTotalCoins();
+        long totalBanknotes = banknotes.getTotalBanknotes();
+        long totalCoins = coins.getTotalCoins();
         long totalPaid = totalBanknotes + totalCoins;
+        stationLogic.productController.subtractFromTotal(totalPaid);
+
+        if (charge < 0){
+            throw new Exception("Unable to complete transaction for the amount: $" + charge);
+        } else if (totalPaid < charge){
+            throw new Exception("Please add amount to complete the payment");
+        }
+
         long change = totalPaid - charge;
         return change;
     }
